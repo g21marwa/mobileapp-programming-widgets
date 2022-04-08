2877bddaafcb0c121b0fdb986994a6cf1a74c3bc
Martin Warg <g21marwa@student.his.se>
2022-04-08 10:52:20 +0200

    Added a onClick event for the button. it advances the progressbar by 10

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ProgressBar myProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myProgress = (ProgressBar)findViewById(R.id.progressbar);
+    }
+    public void changeProgress(View view){
+        int getProgress = myProgress.getProgress();
+        myProgress.setProgress(getProgress+10);
     }
 }
